f30a55fe9630a573e9823b552fb3ecb215f64e09
jianguo-h <1060684755@qq.com>
2018-01-08 21:59:10 +0800

    开发环境搭建 - 3 热加载基本完成

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './App';
+
+if(module.hot) {
+  module.hot.accept();
+}
 
 render(
-  <div>hello react!</div>,
+  <App />,
   document.getElementById('app')
 );
