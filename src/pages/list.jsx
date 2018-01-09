6ae91f22794cf1fe69433eb108d9b94bd3f85ea0
jianguo-h <1060684755@qq.com>
2018-01-10 00:04:17 +0800

    1. koa路由配置  2. 初步增加list组件

---
+++ src/pages/list.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <div id = "content">
+        <div className="list">
+          this is song list
+        </div>
+      </div>
+    );
+  }
+}
+
+export default List;
