ee32b5e4eb97b3d4f82f3eaab474612e8e4241c4
jianguo <1060684755@qq.com>
2018-01-11 23:00:22 +0800

    初步完成头部部分的搭建

---
+++ src/components/drop-list.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class DropList extends Component {
+  static propTypes = {
+    resultCount: PropTypes.number,
+    searchTip: PropTypes.string,
+    resultList: PropTypes.array
+  }
+  static defaultProps = {
+    resultCount: 0,
+    searchTip: '正在搜索...',
+    resultList: []
+  }
+  render() {
+    const { resultCount, searchTip, resultList, search } = this.props;
+    let tipDom = <p>{ searchTip }</p>;
+    let list = [];
+    if(resultCount > 0) {
+      list = resultList.map((item, index) => {
+        return <li key = { index } onClick = { search.bind(this, item.HintInfo) }>{ item.HintInfo }</li>;
+      });
+    }
+    return (
+      <div className = "search-list">
+        { resultCount > 0 ? <ul>{list}</ul> : tipDom }
+      </div>
+    );
+  }
+}
+
+export default DropList;
--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -1,10 +1,73 @@
 import React, { Component } from 'react';
+import DropList from '../components/drop-list';
+import '../less/header.less';
 
+const tabs = [
+  {
+    path: "/new",
+    name: "新歌"
+  },
+  {
+    path: "/recommend",
+    name: "推荐"
+  },
+  {
+    path: "/local",
+    name: "本地"
+  }
+];
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      keyword: '',                // 搜索的关键字
+      resultCount: 0,             // 得到的结果数量
+      resultList: [],             // 搜索得到的结果列表
+      searchTip: '正在搜索...',    // 搜索时的提示信息
+    }
+  }
+  // 监听输入框的input事件
+  input(evt) {
+    this.setState({
+      keyword: evt.target.value
+    });
+    this.query();
+  }
+  // 根据关键字搜索(模糊查询)
+  query() {
+
+  }
+  // 点击搜索事件, keyword为关键字
+  search(keyword) {
+    console.log('>>> keyword', keyword);
+  }
   render() {
+    const keyword = this.state.keyword.trim();
+    const dropListProps = {
+      resultCount: this.state.resultCount,
+      resultList: this.state.resultList,
+      searchTip: this.state.searchTip,
+      search: this.search.bind(this)
+    }
     return (
       <header id = 'header'>
-        this is header
+        <div className = "header-search">
+          <div className = "logo"></div>
+          <div className = "search-form">
+            <input type = 'text' placeholder = "歌手/歌名" value = { keyword } onChange = { this.input.bind(this) } />
+            { keyword ? <DropList { ...dropListProps } /> : null }
+          </div>
+          <div className = "search" onClick = { this.search.bind(this, keyword) }></div>
+        </div>
+        <div className = "header-tab">
+          <ul>
+            {
+              tabs.map((tab, index)  => {
+                return <li key = { index }>{ tab.name }</li>;
+              })
+            }
+          </ul>
+        </div>
       </header>
     );
   }
