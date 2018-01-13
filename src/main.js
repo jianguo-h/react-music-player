6cb51f41d63b2007af3d0a57df3684297c92473e
jianguo <1060684755@qq.com>
2018-01-13 22:39:13 +0800

    渲染 ’新歌模块列表‘ 完成

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+import api from './api';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import adapt from './js/adapt';
+import { Toast } from 'antd-mobile';
 import './less/app.less';
 
 // hot module
@@ -11,6 +13,8 @@ if(module.hot) {
 
 // some config
 adapt();
+window.api = api;
+window.Toast = Toast;
 
 render(
   <App />,
