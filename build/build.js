cd0f9209c4fd92f80553261f96d49414e9856c5e
jianguo <1060684755@qq.com>
2018-01-30 23:39:56 +0800

    增加 webpack打包配置文件

---
+++ build/build.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const rimraf = require("rimraf");
+const webpack = require("webpack");
+const webpackProdConfig = require("./webpack.prod.config");
+
+console.log("building for production...\n");
+const distPath = path.resolve(__dirname, "../dist");
+
+rimraf(distPath, err => {
+  if(err) throw err;
+
+  webpack(webpackProdConfig, (err, stats) => {
+    if(err) throw err;
+    // process.stdout.write(stats.toString({
+    // 	colors: true,
+    // 	modules: false,
+    // 	children: false,
+    // 	chunks: false,
+    // 	chunkModules: false
+    // }) + "\n\n");
+
+    console.log("Build complete \n");
+    console.log(
+      "Tip: built files are meant to be served over an HTTP server.\n" +
+      "Opening index.html over file:// won\'t work.\n'"
+    );
+  });
+});
