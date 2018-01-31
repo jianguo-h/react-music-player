81936c176464968f0d8c308024806f15fb2470aa
jianguo-h <1060684755@qq.com>
2018-01-31 23:58:37 +0800

    打包配置文件完善

--- build/build.js
+++ build/build.js
@@ -1,24 +1,28 @@
 const fs = require("fs");
+const ora = require('ora');
 const path = require("path");
 const rimraf = require("rimraf");
 const webpack = require("webpack");
 const webpackProdConfig = require("./webpack.prod.config");
 
-console.log("building for production...\n");
-const distPath = path.resolve(__dirname, "../dist");
+const spinner = ora('building for production...')
+spinner.start();
 
+const distPath = path.resolve(__dirname, "../dist");
 rimraf(distPath, err => {
   if(err) throw err;
 
   webpack(webpackProdConfig, (err, stats) => {
+    spinner.stop()
     if(err) throw err;
-    // process.stdout.write(stats.toString({
-    // 	colors: true,
-    // 	modules: false,
-    // 	children: false,
-    // 	chunks: false,
-    // 	chunkModules: false
-    // }) + "\n\n");
+    
+    process.stdout.write(stats.toString({
+    	colors: true,
+    	modules: false,
+    	children: false,
+    	chunks: false,
+    	chunkModules: false
+    }) + "\n\n");
 
     console.log("Build complete \n");
     console.log(
