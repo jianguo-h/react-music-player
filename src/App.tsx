9c48ce3bc205b374a207cd34a8911eefaa22362d
jianguo <1060684755@qq.com>
2019-09-29 14:38:14 +0800

    feat: App.jsx -> App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactHeader from './pages/header';
+import Player from './pages/player';
+import SuspendLyric from './pages/suspend-lyric';
+import routes from './routes';
+import { hot } from 'react-hot-loader/root';
+
+function App() {
+  return (
+    <div id="root">
+      <ReactHeader />
+      {routes}
+      <Player />
+      <SuspendLyric />
+    </div>
+  );
+}
+
+export default hot(App);
