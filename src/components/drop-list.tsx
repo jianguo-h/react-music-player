7c42ff82c94cd5860b1ef65f3d627da2af7c1c70
jianguo <1060684755@qq.com>
2019-09-29 17:44:20 +0800

    feat: 部分组件迁移至ts

---
+++ src/components/drop-list.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface IProps {
+  resultCount: number;
+  searchTip: string;
+  resultList: any[];
+  search: (keyword: string) => void;
+}
+
+function DropList(props: IProps) {
+  const {
+    resultCount = 0,
+    searchTip = '正在搜索...',
+    resultList = [],
+    search
+  } = props;
+
+  return (
+    <div className="search-list">
+      {resultCount > 0 ? (
+        <ul>
+          {resultList.map((item, index) => {
+            return (
+              <li key={index} onClick={() => search(item.HintInfo)}>
+                {item.HintInfo}
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>{searchTip}</p>
+      )}
+    </div>
+  );
+}
+
+export default DropList;
