d21e170ecbb9a858fa564e6c2f17117ee0049c0b
jianguo-h <1060684755@qq.com>
2020-01-21 17:33:20 +0800

    refactor: update api/index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,60 +1,58 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+
+async function callApi(requestConfig: AxiosRequestConfig, title: string) {
+  try {
+    console.log('[request.title] ' + title);
+    console.log(requestConfig.data ?? requestConfig.params)
+    return await axios(requestConfig);
+  } catch (err) {
+    throw err;
+  }
+}
 
 // 统一管理各个请求
 export default {
   // 根据关键字搜索(模糊查询用到)
   async search(keyword: string) {
-    try {
-      const params = { keyword };
-      console.log('>>> [api.params] 根据关键字搜索', params);
-      return await axios.post('/searchtip', params);
-    } catch (err) {
-      throw new Error(err);
-    }
+    return await callApi({
+      method: 'post',
+      url: '/searchtip',
+      data: { keyword }
+    }, '搜索');
   },
   // 获取静态json数据中的歌曲列表
   async getList(path: string) {
-    try {
-      console.log('>>> [api.params] 获取静态json数据中的歌曲列表', path);
-      return await axios.post('/api/' + path);
-    } catch (err) {
-      throw new Error(err);
-    }
+    return await callApi({
+      method: 'post',
+      url: '/api/' + path
+    }, '获取静态json数据中的歌曲列表');
   },
   // 获取歌曲的一些信息
   async getSongInfo(songName: string, page: number = 1) {
-    try {
-      const params = {
-        params: {
-          page,
-          pagesize: 20,
-          keyword: songName,
-          platform: 'WebFilter',
-          userid: -1,
-          iscorrection: 1,
-          privilege_filter: 0,
-          filter: 2
-        }
-      };
-      console.log('>>> [api.params] 获取歌曲的一些信息', params);
-      return await axios.get('/songsearch', params);
-    } catch (err) {
-      throw new Error(err);
-    }
+    return await callApi({
+      method: 'get',
+      url: '/songsearch',
+      params: {
+        page,
+        pagesize: 20,
+        keyword: songName,
+        platform: 'WebFilter',
+        userid: -1,
+        iscorrection: 1,
+        privilege_filter: 0,
+        filter: 2
+      }
+    }, '获取歌曲信息');
   },
   // 根据hash值获取歌曲的信息
   async play(hash: string) {
-    try {
-      const params = {
-        params: {
-          r: 'play/getdata',
-          hash
-        }
-      };
-      console.log('>>> [api.params] 根据hash值获取歌曲的信息', params);
-      return await axios.get('/play', params);
-    } catch (err) {
-      throw new Error(err);
-    }
+    return await callApi({
+      method: 'get',
+      url: '/play',
+      params: {
+        r: 'play/getdata',
+        hash
+      }
+    }, '根据hash值获取歌曲的信息');
   }
 };
