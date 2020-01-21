4a4929789bc4511cc259443b756a331c7f88a490
jianguo-h <1060684755@qq.com>
2020-01-21 16:06:46 +0800

    feat: main.js -> mian.tsx

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import store from './store';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import adapt from './ts/adapt';
+import { HashRouter as Router } from 'react-router-dom';
+import './less/app.less';
+
+// hot module
+if (module.hot) {
+  module.hot.accept();
+}
+
+// some config
+adapt();
+
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
