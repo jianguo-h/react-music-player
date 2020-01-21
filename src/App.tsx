6e1314ecc1b0b38a9260d2e4582027ae45b456af
jianguo-h <1060684755@qq.com>
2020-01-21 15:43:39 +0800

    feat: update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import ReactHeader from './pages/header';
 import Player from './pages/player';
+import List from './pages/list';
 import SuspendLyric from './pages/suspend-lyric';
-import routes from './routes';
 import { hot } from 'react-hot-loader/root';
 
 function App() {
   return (
     <div id="root">
       <ReactHeader />
-      {routes}
+      <Switch>
+        <Route path='/new' component={List} />
+        <Route path='/recommend' component={List} />
+        <Route path='/local' component={List} />
+        <Route path='/search/:keyword' component={List} />
+        <Redirect to='/new' />
+      </Switch>
       <Player />
       <SuspendLyric />
     </div>
