bfb64e8ff7968eede8e7f26b9d1851e8af99296e
jianguo-h <1060684755@qq.com>
2020-03-20 15:54:24 +0800

    style: clean unused code

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,24 +1,5 @@
 import { IRootState } from './index';
 
-/* export interface IRootState {
-  view: string;
-  songList: any[];
-  searchListCount: number;
-  audio: AudioEle;
-  audioSrc: string | null;
-  isPlayed: boolean;
-  canPlayed: boolean;
-  paused: boolean;
-  curPlaySong: IPlaySongInfo;
-  curPlayImgSrc: string;
-  curPlayLrcArr: any[];
-  lock: boolean;
-  loop: boolean;
-  modeType: string;
-  lrcConfig: ILrcConfig;
-  lrcSwitch: boolean;
-} */
-
 export interface IPlaySongInfo {
   index: number;
   FileName: string;
