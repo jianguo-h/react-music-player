3237f97a97ddb884b4c1eeb8597bb6b6e7e921c8
jianguo-h <1060684755@qq.com>
2020-03-20 12:47:46 +0800

    feat: update reducer to ts

---
+++ src/store/actions/index.ts
@@ -0,0 +1,278 @@
+// action creators function
+import { getSongInfo as apiGetSongInfo, play as apiPlay } from '@src/api';
+import { Toast } from 'antd-mobile';
+import _cloneDeep from 'lodash/cloneDeep';
+import {
+  IAction,
+  ILrcConfig,
+  IPlaySongInfo,
+  GetStateFn,
+  AudioEle
+} from '../types';
+import { Dispatch } from 'redux';
+
+export const SET_VIEW = 'SET_VIEW';
+export function setView(view: string): IAction {
+  return {
+    type: SET_VIEW,
+    payload: {
+      view
+    }
+  };
+}
+
+export const SET_SONG_LIST = 'SET_SONG_LIST';
+export function setSongList(songList: any[]): IAction {
+  return {
+    type: 'SET_SONG_LIST',
+    payload: {
+      songList
+    }
+  };
+}
+
+export const SET_SEARCH_LIST_COUNT = 'SET_SEARCH_LIST_COUNT';
+export function setSearchListCount(searchListCount: number): IAction {
+  return {
+    type: SET_SEARCH_LIST_COUNT,
+    payload: {
+      searchListCount
+    }
+  };
+}
+
+export const SET_AUDIO = 'SET_AUDIO';
+export function setAudio(audio: AudioEle): IAction {
+  return {
+    type: SET_AUDIO,
+    payload: {
+      audio
+    }
+  };
+}
+
+export const SET_AUDIO_SRC = 'SET_AUDIO_SRC';
+export function setAudioSrc(audioSrc: string | null): IAction {
+  return {
+    type: SET_AUDIO_SRC,
+    payload: {
+      audioSrc
+    }
+  };
+}
+
+export const SET_IS_PLAYED = 'SET_IS_PLAYED';
+export function setIsPlayed(isPlayed: boolean): IAction {
+  return {
+    type: SET_IS_PLAYED,
+    payload: {
+      isPlayed
+    }
+  };
+}
+
+export const SET_CAN_PLAYED = 'SET_CAN_PLAYED';
+export function setCanPlayed(canPlayed: boolean): IAction {
+  return {
+    type: SET_CAN_PLAYED,
+    payload: {
+      canPlayed
+    }
+  };
+}
+
+export const SET_PAUSED = 'SET_PAUSED';
+export function setPaused(paused?: boolean): IAction {
+  if (paused !== undefined) {
+    return {
+      type: SET_PAUSED,
+      payload: {
+        paused
+      }
+    };
+  }
+  return {
+    type: SET_PAUSED,
+    payload: {}
+  };
+}
+
+export const SET_CUR_PLAY_IMR_SRC = 'SET_CUR_PLAY_IMR_SRC';
+export function setCurPlayImgSrc(curPlayImgSrc: string): IAction {
+  return {
+    type: SET_CUR_PLAY_IMR_SRC,
+    payload: {
+      curPlayImgSrc
+    }
+  };
+}
+
+export const SET_CUR_PLAY_LRC_ARR = 'SET_CUR_PLAY_LRC_ARR';
+export function setCurPlayLrcArr(lyrics: any[]): IAction {
+  return {
+    type: SET_CUR_PLAY_LRC_ARR,
+    payload: {
+      lyrics
+    }
+  };
+}
+
+export const SET_LOCK = 'SET_LOCK';
+export function setLock(lock: boolean): IAction {
+  return {
+    type: SET_LOCK,
+    payload: {
+      lock
+    }
+  };
+}
+
+export const SET_LOOP = 'SET_LOOP';
+export function setLoop(loop: boolean): IAction {
+  return {
+    type: SET_LOOP,
+    payload: {
+      loop
+    }
+  };
+}
+
+export const SET_MODE_TYPE = 'SET_MODE_TYPE';
+export function setModeType(modeType: string): IAction {
+  return {
+    type: SET_MODE_TYPE,
+    payload: {
+      modeType
+    }
+  };
+}
+
+export const SET_LRC_CONFIG = 'SET_LRC_CONFIG';
+export function setLrcConfig(lrcConfig: ILrcConfig): IAction {
+  return {
+    type: SET_LRC_CONFIG,
+    payload: {
+      lrcConfig
+    }
+  };
+}
+
+export const SET_LRC_SWITCH = 'SET_LRC_SWITCH';
+export function setLrcSwitch(lrcSwitch: boolean): IAction {
+  return {
+    type: SET_LRC_SWITCH,
+    payload: {
+      lrcSwitch
+    }
+  };
+}
+
+export const SET_CUR_PLAY_SONG = 'SET_CUR_PLAY_SONG';
+export function setCurPlaySong(curPlaySong: IPlaySongInfo): IAction {
+  return {
+    type: SET_CUR_PLAY_SONG,
+    payload: {
+      curPlaySong
+    }
+  };
+}
+
+export function togglePlayStatus() {
+  return (dispatch: Dispatch<any>, getState: GetStateFn) => {
+    const state = getState();
+    if (!state.audioSrc) {
+      dispatch(setPaused(false));
+      return;
+    }
+    dispatch(setPaused());
+
+    const { audio, paused } = getState();
+    if (paused) {
+      audio?.pause();
+    } else {
+      audio?.pause();
+    }
+  };
+}
+
+export function playSong(curPlayIndex: number) {
+  return async (dispatch: Dispatch<any>, getState: GetStateFn) => {
+    Toast.loading('加载中...', 0);
+
+    dispatch(setIsPlayed(false));
+    dispatch(setAudioSrc(''));
+    dispatch(setCurPlayLrcArr([]));
+    dispatch(
+      setCurPlayImgSrc(require('../../static/images/singer-default.jpg'))
+    );
+    dispatch(togglePlayStatus());
+    dispatch(setLock(false));
+    dispatch(
+      setLrcConfig({
+        activeLrcIndex: 0
+      })
+    );
+
+    const state = getState();
+    const songList = _cloneDeep(state.songList);
+    if (curPlayIndex < 0) {
+      curPlayIndex = songList.length - 1;
+    } else if (curPlayIndex >= songList.length) {
+      curPlayIndex = 0;
+    }
+    const curPlaySong = _cloneDeep(songList[curPlayIndex]);
+    curPlaySong.index = curPlayIndex;
+    dispatch(setCurPlaySong(curPlaySong));
+
+    try {
+      const songName = curPlaySong.FileName;
+      const infoRes = await apiGetSongInfo(songName);
+      console.log('>>> [res] 获取歌曲的一些信息', infoRes);
+      if (infoRes.status === 200 && infoRes.statusText === 'OK') {
+        let rightSong: any = '';
+        for (const song of infoRes.data.data.lists) {
+          if (song.FileName === songName) {
+            rightSong = _cloneDeep(song);
+            break;
+          }
+        }
+        const hash = rightSong.FileHash;
+        try {
+          const playRes = await apiPlay(hash);
+          console.log('>>> [res] 根据hash值获取歌曲的播放信息', playRes);
+          if (playRes.status === 200 && playRes.statusText === 'OK') {
+            const data = _cloneDeep(playRes.data.data);
+            Toast.hide();
+            if (!data.play_url) {
+              Toast.fail('暂无播放来源');
+              return;
+            }
+            const audioSrc = data.play_url;
+            const curPlayImgSrc = data.img;
+            const lyrics = data.lyrics;
+            dispatch(setCanPlayed(true));
+            dispatch(setAudioSrc(audioSrc));
+            dispatch(setCurPlayLrcArr(lyrics));
+            dispatch(setCurPlayImgSrc(curPlayImgSrc));
+          } else {
+            Toast.hide();
+            Toast.fail('播放歌曲失败');
+            return;
+          }
+        } catch (err) {
+          Toast.hide();
+          console.log('>>> [err] 获取歌曲的信息', err);
+          Toast.fail('网络出现错误或服务暂时不可用');
+        }
+      } else {
+        Toast.hide();
+        Toast.fail('播放歌曲失败');
+        return;
+      }
+    } catch (err) {
+      Toast.hide();
+      Toast.fail('网络出现错误或服务暂时不可用');
+      throw new Error(err);
+    }
+  };
+}
---
+++ src/store/reducers/index.ts
@@ -0,0 +1,237 @@
+// reducers
+import _cloneDeep from 'lodash/cloneDeep';
+import { IAction, IPlaySongInfo, ILrcConfig, AudioEle } from '../types';
+import {
+  SET_VIEW,
+  SET_SONG_LIST,
+  SET_SEARCH_LIST_COUNT,
+  SET_AUDIO,
+  SET_AUDIO_SRC,
+  SET_IS_PLAYED,
+  SET_CAN_PLAYED,
+  SET_PAUSED,
+  SET_CUR_PLAY_IMR_SRC,
+  SET_CUR_PLAY_LRC_ARR,
+  SET_CUR_PLAY_SONG,
+  SET_LOCK,
+  SET_LOOP,
+  SET_MODE_TYPE,
+  SET_LRC_CONFIG,
+  SET_LRC_SWITCH
+} from '../actions';
+
+// 当前播放歌曲所属的路由
+export const view = (state: string = '', action: IAction): string => {
+  if (action.type === SET_VIEW) {
+    return action.payload.view;
+  }
+  return state;
+};
+
+// 当前播放歌曲所属的歌词列表
+export const songList = (state: any[] = [], action: IAction): any[] => {
+  if (action.type === SET_SONG_LIST) {
+    return action.payload.songList;
+  }
+  return state;
+};
+
+// 搜索框搜索后得到的歌曲数量
+export const searchListCount = (state: number = 0, action: IAction): number => {
+  if (action.type === SET_SEARCH_LIST_COUNT) {
+    return action.payload.searchListCount;
+  }
+  return state;
+};
+
+// audio的dom节点
+export const audio = (state: AudioEle = null, action: IAction): AudioEle => {
+  if (action.type === SET_AUDIO) {
+    return action.payload.audio;
+  }
+  return state;
+};
+
+// audioSrc播放来源
+export const audioSrc = (
+  state: string | null = null,
+  action: IAction
+): string | null => {
+  if (action.type === SET_AUDIO_SRC) {
+    return action.payload.audioSrc;
+  }
+  return state;
+};
+
+// 是否有音乐在播放
+export const isPlayed = (state: boolean = false, action: IAction): boolean => {
+  if (action.type === SET_IS_PLAYED) {
+    return action.payload.isPlayed;
+  }
+  return state;
+};
+
+// 是否可以播放音乐
+export const canPlayed = (state: boolean = false, action: IAction): boolean => {
+  if (action.type === SET_CAN_PLAYED) {
+    return action.payload.canPlayed;
+  }
+  return state;
+};
+
+// 音频是否为暂停状态
+export const paused = (state: boolean = false, action: IAction): boolean => {
+  if (action.type === SET_PAUSED) {
+    if (action.payload.paused !== undefined) {
+      return action.payload.paused;
+    }
+    return !state;
+  }
+  return state;
+};
+
+/* const defaultCurPlaySong = {
+  el: null,
+  src: '',
+  index: -1,
+  FileName: '',
+  SongName: '',
+  SingerName: '',
+  SingerImg: '/singer-default.jpg',
+  paused: false,
+  modeType: 'order',
+  lrcArr: [],
+  lrcIndex: 0,
+  lrcColor: {
+    defaultColor: "#b2f5b5",
+    activeColor: "#d1c90e"
+  },
+  loop: false,
+}
+export const curPlaySong = (state = defaultCurPlaySong, action) => {
+  switch (action.type) {
+    case 'setSongInfo':
+      return {
+        ...state,
+        ...action.songInfo
+      }
+    case 'setPaused':
+      return {
+        ...state,
+        action.
+      }
+    default:
+      return state;
+  }
+} */
+const defaultCurPlaySong: IPlaySongInfo = {
+  index: -1, // 当前播放歌曲的索引
+  FileName: '', // 当前播放歌曲的全名
+  SongName: '', // 当前播放歌曲的歌曲名
+  SingerName: '' // 当前播放歌曲的歌手名
+};
+export const curPlaySong = (
+  state: IPlaySongInfo = defaultCurPlaySong,
+  action: IAction
+): IPlaySongInfo => {
+  if (action.type === SET_CUR_PLAY_SONG) {
+    return {
+      ...state,
+      ...action.payload.curPlaySong
+    };
+  }
+  return state;
+};
+
+// 歌手图片来源, 默认值
+export const curPlayImgSrc = (
+  state: string = require('../../static/images/singer-default.jpg'),
+  action: IAction
+): string => {
+  if (action.type === SET_CUR_PLAY_IMR_SRC) {
+    return action.payload.curPlayImgSrc;
+  }
+  return state;
+};
+
+// 歌词数组
+export const curPlayLrcArr = (state: any[] = [], action: IAction): any[] => {
+  if (action.type === SET_CUR_PLAY_LRC_ARR) {
+    const lyrics = _cloneDeep(action.payload.lyrics);
+    if (lyrics.length === 0) {
+      return state;
+    }
+    const lrc = lyrics
+      .replace(/\n/g, '')
+      .split('[')
+      .slice(1);
+    const curPlayLrcArr = [];
+    for (const [index, item] of lrc.entries()) {
+      const times = item
+        .split(']')[0]
+        .replace('.', ':')
+        .split(':');
+      const time =
+        Number(times[0]) * 60 + Number(times[1]) + Number(times[2]) / 1000;
+      const obj = {
+        index,
+        startTime: time.toFixed(2),
+        curLrc: item.split(']')[1]
+      };
+      curPlayLrcArr.push(obj);
+    }
+    return curPlayLrcArr;
+  }
+  return state;
+};
+
+// 事件开关, 防止canplay事件多次执行
+export const lock = (state: boolean = false, action: IAction): boolean => {
+  if (action.type === SET_LOCK) {
+    return action.payload.lock;
+  }
+  return state;
+};
+
+// 歌曲是否循环播放
+export const loop = (state: boolean = false, action: IAction): boolean => {
+  if (action.type === SET_LOOP) {
+    return action.payload.loop;
+  }
+  return state;
+};
+
+// 播放模式
+export const modeType = (state: string = 'order', action: IAction): string => {
+  if (action.type === SET_MODE_TYPE) {
+    return action.payload.modeType;
+  }
+  return state;
+};
+
+// 歌词的默认配置
+const defaultLrcConfig: ILrcConfig = {
+  activeColor: '#d1c90e', // 高亮行的歌词颜色
+  defaultColor: '#b2f5b5', // 其他行的歌词颜色
+  activeLrcIndex: 0 // 高亮行歌词的索引
+};
+export const lrcConfig = (
+  state: ILrcConfig = defaultLrcConfig,
+  action: IAction
+): ILrcConfig => {
+  if (action.type === SET_LRC_CONFIG) {
+    return {
+      ...state,
+      ...action.payload.lrcConfig
+    };
+  }
+  return state;
+};
+
+// 是否显示悬浮歌词
+export const lrcSwitch = (state: boolean = false, action: IAction): boolean => {
+  if (action.type === SET_LRC_SWITCH) {
+    return action.payload.lrcSwitch;
+  }
+  return state;
+};
