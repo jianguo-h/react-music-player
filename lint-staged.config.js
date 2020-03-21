7badb3e5d764468ee3d3a682611c9d257ba2a407
jianguo-h <1060684755@qq.com>
2020-03-21 18:10:28 +0800

    feat: add style lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,6 @@ module.exports = {
   'src/**/*.{js?(x),ts?(x)}': [
     'prettier --ignore-path .eslintignore --write',
     'eslint --fix'
-  ]
+  ],
+  'src/**/*.{css,less,scss,sass}': ['stylelint --fix']
 };
