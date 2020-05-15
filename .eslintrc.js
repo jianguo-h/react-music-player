9d965790d846fe345b3ff03e5426dcd51d520d72
jianguo.huang <jianguo.huang@nautiluscapital.net>
2020-05-15 13:54:43 +0800

    chore: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,44 +7,48 @@ module.exports = {
     'prettier',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
-    'prettier/react'
+    'prettier/react',
   ],
+  plugins: ['react', 'react-hooks'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2019,
     ecmaFeatures: {
       jsx: true,
-    }
+    },
   },
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
-  plugins: [
-    'react',
-    'react-hooks'
-  ],
   settings: {
     react: {
       pragma: 'React',
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   rules: {
     '@typescript-eslint/no-var-requires': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/no-inferrable-types': ['error', {
-      ignoreParameters: true
-    }]
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      {
+        ignoreParameters: true,
+      },
+    ],
+    '@typescript-eslint/camelcase': [
+      'error',
+      { properties: 'never', ignoreDestructuring: true },
+    ],
   },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        'react/prop-types': 'off'
-      }
-    }
-  ]
+        'react/prop-types': 'off',
+      },
+    },
+  ],
 };
