200609cf40268d739398d0d8c71078366ab5c911
jianguo.huang <jianguo.huang@nautiluscapital.net>
2020-05-15 14:25:09 +0800

    style: rename

--- koa/static.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const koaStatic = require('koa-static');
-
-module.exports = (app, filePath) => {
-  const staticPath = path.resolve(__dirname, filePath);
-  app.use(koaStatic(staticPath));
-};
---
+++ server/index.js
@@ -0,0 +1,38 @@
+const Koa = require('koa');
+const path = require('path');
+const send = require('koa-send');
+const proxy = require('./proxy');
+const configStatic = require('./static');
+const router = require('./router');
+const config = require('../config');
+const detectionPort = require('./detection-port');
+
+const app = new Koa();
+const serverPort = config.prod.port;
+
+// config koa router
+router(app);
+
+// config koa proxy
+proxy(app);
+
+// config static file
+configStatic(app, '../dist');
+
+// config dist directory start
+app.use(async ctx => {
+  // 这样写也可以
+  /*if(ctx.path === '/') {
+    const fs = require('fs');
+    ctx.response.type = 'html';
+    ctx.response.body = fs.createReadStream(path.resolve(__dirname, '../dist/index.html'));
+  }*/
+  if (ctx.path === '/') {
+    await send(ctx, ctx.path, {
+      root: path.resolve(__dirname, '../dist'),
+    });
+  }
+});
+
+// 端口检测
+detectionPort(app, serverPort);
