302b202e46e0f8576696d78a951d6899ab421bfb
jianguo-h <1060684755@qq.com>
2020-05-15 14:25:09 +0800

    style: rename

---
+++ server/router.js
@@ -0,0 +1,18 @@
+const songData = require('../src/static/data/song.json');
+const Router = require('koa-router');
+const router = new Router();
+
+// config koa router
+module.exports = app => {
+  const paths = ['new', 'recommend', 'local'];
+  for (const path of paths) {
+    router.post('/api/' + path, async (ctx, next) => {
+      ctx.response.status = 200;
+      ctx.response.body = {
+        // path: route,
+        data: songData[path],
+      };
+    });
+  }
+  app.use(router.routes()).use(router.allowedMethods());
+};
